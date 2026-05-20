65710e95612980561231a84e38a86ab3d01b8d73
PeterTZN <jeep11@hotmail.com>
2026-05-20 17:14:31 +0100

    finish front end file, added bootstrap classes/formatting.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        date = request.form['date']
+        occupation = request.form['occupation']
+
+
+    return render_template('index.html')
 
 
 app.run(debug=True, port=5001)
