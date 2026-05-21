04c4dbe29b6131f4ca8e786ac75560ea2aa5ed17
PeterTZN <jeep11@hotmail.com>
2026-05-21 13:05:22 +0100

    Added completion message to page, shows form was submitted successfully.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -32,6 +32,7 @@ def index():
                     date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash(f"{first_name}, Your form was submitted successfully!", "success")
 
     return render_template('index.html')
 
